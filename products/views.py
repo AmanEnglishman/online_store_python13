5f1caa3ccb52a9073a2c851c28640c98f59bfbc5
electrovenik <englishmanaman@gmail.com>
2026-03-09 17:28:19 +0600

    asd

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product, Category
+
+def home(request):
+    products = Product.objects.prefetch_related('images').all[:12]
+    categories = Category.objects.filter(parent=None)
+
+    context = {
+        'products': products,
+        'categories': categories
+    }
+
+    return render(request, 'home.html', context)
