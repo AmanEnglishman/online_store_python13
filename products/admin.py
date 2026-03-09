09650e77f296c96ee2a4efb5d1f3aa8e9278b394
electrovenik <englishmanaman@gmail.com>
2026-03-09 16:58:03 +0600

    first

---
+++ products/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+from .models import Product, Category, ProductImage, ProductSpecification
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent')
+    search_fields = ('name',)
+    list_filter = ('parent',)
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+class ProductSpecificationInline(admin.TabularInline):
+    model = ProductSpecification
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price',)
+    search_fields = ('name', 'description',)
+    list_filter = ('category', 'created_at',)
+
+    inlines = [ProductImageInline, ProductSpecificationInline]
+
+    readonly_fields = ('created_at',)
---
+++ products/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='subcategories',
+    )
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products',
+    )
+    stock = models.PositiveSmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='images',
+    )
+    image = models.ImageField(upload_to='images/')
+    is_main = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Image for {self.product.name}'
+
+class ProductSpecification(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=255)
+    value = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
