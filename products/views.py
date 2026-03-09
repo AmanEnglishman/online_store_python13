039f337ec061ffd58ff88b20f12be8db75743005
electrovenik <englishmanaman@gmail.com>
2026-03-09 17:46:24 +0600

    html css

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Product, Category
 
 def home(request):
-    products = Product.objects.prefetch_related('images').all[:12]
+    products = Product.objects.prefetch_related('images').all()[:12]
     categories = Category.objects.filter(parent=None)
 
     context = {
